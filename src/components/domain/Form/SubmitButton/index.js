8d2c1c84c4a9cf5acbfdc98550eec6e876f112e7
waterglasses <hsk10271@gmail.com>
2022-06-14 07:01:15 +0900

    feat: Login 컴포넌트 구현

---
+++ src/components/domain/Form/SubmitButton/index.js
@@ -0,0 +1,17 @@
+import { Button } from '@components'
+
+const SubmitButton = ({ children }) => {
+  const submitButtonStyle = {
+    fontSize: '16px',
+    height: '40px',
+    borderRadius: '4px',
+    boxSizing: 'border-box',
+    border: 'none',
+    backgroundColor: '#9e7373',
+    color: 'white',
+  }
+
+  return <Button style={submitButtonStyle}>{children}</Button>
+}
+
+export default SubmitButton
