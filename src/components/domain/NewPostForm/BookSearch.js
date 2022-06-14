c90069b3d5fe8084bb1ffae8e120a256b7fa146f
yong <tkddbs1644@gmail.com>
2022-06-14 17:49:41 +0900

    feat: 책 선택 기능 구현

--- src/components/domain/NewPostForm/BookSearch.js
+++ src/components/domain/NewPostForm/BookSearch.js
@@ -1,7 +1,9 @@
 import styled from '@emotion/styled'
 import { Input, Button, Title, Image } from '@components'
+import { useState } from 'react'
+import { getBookList } from '@utils/api/getBookList'
 
-const defaultImageUrl = 'https://source.unsplash.com/random'
+const DEFAULT_IMAGE_URL = 'https://source.unsplash.com/random'
 
 const BookSearchContainer = styled.div`
   display: flex;
@@ -28,6 +30,25 @@ const BookList = styled.ul`
 `
 
 const BookSearch = () => {
+  const [imgUrl, setImgUrl] = useState(DEFAULT_IMAGE_URL)
+  const [keyword, setKeyword] = useState('')
+  const [bookList, setBookList] = useState([])
+
+  const onChangeKeyword = (e) => {
+    setBookList([])
+    setImgUrl(DEFAULT_IMAGE_URL)
+    setKeyword(e.target.value)
+  }
+
+  const onSearch = async (e) => {
+    e.preventDefault()
+    if (!keyword) return
+    const bookList = await getBookList(keyword)
+    setBookList([...bookList.documents])
+  }
+
+  const onChangeImage = (e) => setImgUrl(e.target.value)
+
   return (
     <>
       <BookSearchContainer>
@@ -35,20 +56,28 @@ const BookSearch = () => {
           <Title level={2} style={{ margin: 0 }}>
             책을 선택해주세요!
           </Title>
-          <Image
-            src={defaultImageUrl}
-            width={150}
-            height={200}
-            placeholder="책 이미지"
-            alt="책 제목"
-          />
+          <Image src={imgUrl} width={150} height={200} placeholder="책 이미지" alt="책 제목" />
         </LeftBox>
         <RightBox>
           <SearchBar>
-            <Input placeholder="책을 검색해주세요." block required />
-            <Button>검색</Button>
+            <Input placeholder="책을 검색해주세요." block required onChange={onChangeKeyword} />
+            <Button onClick={onSearch}>검색</Button>
           </SearchBar>
-          <BookList>책 리스트...</BookList>
+          <BookList>
+            {bookList.map((book, index) => (
+              <li key={index}>
+                <label>
+                  <input
+                    type="radio"
+                    name="book-title"
+                    value={book.thumbnail}
+                    onClick={onChangeImage}
+                  />
+                  {book.title}
+                </label>
+              </li>
+            ))}
+          </BookList>
         </RightBox>
       </BookSearchContainer>
     </>
