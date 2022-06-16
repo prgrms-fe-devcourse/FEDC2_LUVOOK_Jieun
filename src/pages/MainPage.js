4a4177d73587169e6215e92b08e209f703508fba
Daeun Kim <o_o___t@pusan.ac.kr>
2022-06-17 02:22:49 +0900

    feat: 카테고리 세부 검색 기능 구현

--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -9,35 +9,55 @@ const CATEGORY = {
   POET: '시',
 }
 
+const SEARCH_TYPE = {
+  ALL: '전체 검색',
+  TITLE: '책 제목',
+  CONTENT: '내용',
+  QUOTE: '구절',
+}
 const SearchBar = styled.div`
   display: flex;
   justify-content: center;
 `
 
+const sortByLatest = (post1, post2) => {
+  return Date.parse(post2.createdAt) - Date.parse(post1.createdAt)
+}
+
+const parseListTitle = (postList) => {
+  try {
+    return postList.map((post) => {
+      return {
+        ...post,
+        title: JSON.parse(post.title),
+      }
+    })
+  } catch (e) {
+    // TODO: 현재 api 데이터의 title이 JSON.stringify 형태가 아니기 때문에,
+    // 오류가 발생하므로, try-catch 사용
+    return postList
+  }
+}
 const MainPage = () => {
   const [postList, setPostList] = useState([])
   const [categoryName, setCategoryName] = useState(CATEGORY['ALL'])
   const [searchedKeyword, setSearchedKeyword] = useState('')
+  const [searchType, setSearchType] = useState(SEARCH_TYPE['ALL'])
 
   const getAllPost = async () => {
     const channelList = await getChannelList()
-
     const totalPostList = (
       await Promise.all(channelList.map(async (channel) => await getPostListInChannel(channel._id)))
     ).flat()
 
-    totalPostList.sort((post1, post2) => {
-      return Date.parse(post2.createdAt) - Date.parse(post1.createdAt)
-    })
-
-    setPostList(totalPostList)
-    // console.log(totalPostList)
+    totalPostList.sort(sortByLatest)
+    setPostList(parseListTitle(totalPostList))
   }
 
   const getChannelPost = async (channelName) => {
     const channelInfo = await getChannelInfo(channelName)
     const channelPostList = await getPostListInChannel(channelInfo._id)
-    setPostList(channelPostList)
+    setPostList(parseListTitle(channelPostList))
   }
 
   // TODO: 상세 포스트 모달 띄우는 로직
@@ -75,11 +95,34 @@ const MainPage = () => {
     const searchedBookResult = searchedResult.filter((result) => !result.role)
 
     if (categoryName === CATEGORY['ALL']) {
-      setPostList(searchedBookResult)
+      setPostList(parseListTitle(searchedBookResult).sort(sortByLatest))
       return
     } else {
       const { _id } = await getChannelInfo(categoryName)
-      setPostList(searchedBookResult.filter((result) => result.channel === _id))
+      const filteredResult = searchedBookResult.filter((result) => result.channel === _id)
+      setPostList(parseListTitle(filteredResult))
+    }
+  }
+
+  const searchByType = (searchedResult) => {
+    switch (searchType) {
+      case SEARCH_TYPE['ALL']:
+        return searchedResult.filter(
+          (post) =>
+            post.title.bookTitle.includes(searchedKeyword) ||
+            post.title.postContent.includes(searchedKeyword) ||
+            post.title.postQuote.includes(searchedKeyword)
+        )
+      case SEARCH_TYPE['TITLE']:
+        return searchedResult.filter((post) => {
+          return post.title.bookTitle.includes(searchedKeyword)
+        })
+      case SEARCH_TYPE['CONTENT']:
+        return searchedResult.filter((post) => post.title.postContent.includes(searchedKeyword))
+      case SEARCH_TYPE['QUOTE']:
+        return searchedResult.filter((post) => post.title.postQuote.includes(searchedKeyword))
+      default:
+        return searchedResult
     }
   }
 
@@ -89,8 +132,14 @@ const MainPage = () => {
       <Banner />
       <SearchBar>
         <Select
+          data={Object.values(SEARCH_TYPE)}
+          placeholder={'검색 옵션 지정'}
+          onChange={(e) => {
+            setSearchType(e.target.value)
+          }}
+        />
+        <Select
           data={Object.values(CATEGORY)}
-          categories={CATEGORY}
           onChange={(e) => {
             setCategoryName(e.target.value)
           }}
@@ -109,7 +158,6 @@ const MainPage = () => {
         />
         <Button onClick={onSearch}>검색</Button>
       </SearchBar>
-
       <BookListSlider
         posts={postList}
         grid={{ fill: 'row', rows: 2 }}
