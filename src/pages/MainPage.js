f95034fb3450168f99ccce0c977ca83f478258bd
Daeun Kim <o_o___t@pusan.ac.kr>
2022-06-19 21:59:53 +0900

    feat: 북마크 기능 구현

--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -92,6 +92,7 @@ const MainPageSelect = styled(Select)`
 `
 
 const MainPage = () => {
+  const [isRerender, setIsRerender] = useState(true)
   const [postList, setPostList] = useState([])
   const [categoryName, setCategoryName] = useState(CATEGORY_ALL.name)
   const [searchedKeyword, setSearchedKeyword] = useState('')
@@ -152,12 +153,16 @@ const MainPage = () => {
   }
 
   useEffect(() => {
+    if (!isRerender) return
+
     if (categoryName === 'ALL') {
       getAllPost()
     } else {
       getChannelPost(categoryName)
     }
-  }, [categoryName])
+
+    setIsRerender(false)
+  }, [categoryName, isRerender])
 
   useEffect(() => {
     getAllChannels()
@@ -217,7 +222,10 @@ const MainPage = () => {
           }}
           activeItemStyle={{ ...activeItemStyle }}
           items={allCategories}
-          handleClick={(category) => setCategoryName(category.name)}
+          handleClick={(category) => {
+            setCategoryName(category.name)
+            setIsRerender(true)
+          }}
           style={{ margin: '0 200px' }}
         />
         <SearchBar>
@@ -256,7 +264,12 @@ const MainPage = () => {
       </MainPageSection>
 
       <Modal visible={showPostModal} onClose={closePostModal}>
-        <Post post={post} />
+        <Post
+          post={post}
+          handleRerenderPost={() => {
+            setIsRerender(true)
+          }}
+        />
       </Modal>
 
       <Modal
